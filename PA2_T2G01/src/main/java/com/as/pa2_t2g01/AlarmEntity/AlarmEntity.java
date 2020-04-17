45d7162876afe9accd4d60b04ce50c723892d943
user <rafaelgteixeira@ua.pt>
2020-04-17 16:59:04 +0100

    Alarm entity done

--- PA2_T2G01/src/main/java/com/as/pa2_t2g01/AlarmEntity/AlarmEntity.java
+++ PA2_T2G01/src/main/java/com/as/pa2_t2g01/AlarmEntity/AlarmEntity.java
@@ -1,11 +1,77 @@
-package com.as.pa2_t2g01.AlarmEntity;
+package main.java.com.as.pa2_t2g01.AlarmEntity;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+import main.java.com.as.pa2_t2g01.Data.Message;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 
 /**
  * 
  * 
  */
 public class AlarmEntity {
-    public static void main(String[] args) {
+    private static final String kafka_server = "localhost:9092";
+    private static final String create_topics = "false";
+    private static final String enable_commit = "false";
+    private static final String max_records = "50";
+    private static final String alarm_topic = "AlarmTopic";
+    
+    
+    public static void main(String[] args) throws IOException {
         
+        Properties props = new Properties();
+        props.put("bootstrap.servers", kafka_server);
+        props.put("allow.auto.create.topics", create_topics);
+        props.put("enable.auto.commit", enable_commit);
+        props.put("max.poll.records", max_records);
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "MessageDeserializer");
+        
+        KafkaConsumer<String, Message> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(alarm_topic));
+        
+        AlarmGUI gui = new AlarmGUI();
+        gui.startGUI(gui);
+        
+        FileWriter alarmFile = new FileWriter("ALARM.TXT");
+        
+        boolean alarm = false;
+        
+        while (true) {
+            ConsumerRecords<String, Message> records = consumer.poll(Duration.ofMillis(100));
+            
+            for (ConsumerRecord<String, Message> record : records){
+                Message m = record.value();
+                
+                if(m.getMessage_type()== 1){
+                    consumer.commitSync(
+                            Collections.singletonMap(
+                                    new TopicPartition(record.topic(), record.partition()),
+                                    new OffsetAndMetadata(record.offset() + 1)));
+                    if(m.getSpeed() > 120){
+                        if(!alarm){
+                            alarm = true;
+                            alarmFile.write("----------ALARM TURNED ON BY MESSAGE: ");
+                            gui.changeAlarm(alarm);
+                        }
+                        gui.updateTextArea(m.toString());
+                    }
+                    if(alarm && m.getSpeed() < 120){
+                        alarm = false;
+                        gui.changeAlarm(alarm);
+                    }
+                }
+                alarmFile.write(m.toString()+"\n");
+            }
+            consumer.commitSync();
+        }
     }
 }
---
+++ PA2_T2G01/src/main/java/com/as/pa2_t2g01/AlarmEntity/AlarmGUI.java
@@ -0,0 +1,154 @@
+package main.java.com.as.pa2_t2g01.AlarmEntity;
+
+
+public class AlarmGUI extends javax.swing.JFrame {
+
+    public AlarmGUI() {
+        initComponents();
+    }
+    
+    public void updateTextArea(String text) {
+        jTextArea3.append(text);
+        jTextArea3.setCaretPosition(jTextArea3.getText().length());
+    }
+    
+    public void changeAlarm(boolean change){
+        if(change){
+            jLabel1.setText("On");
+        }
+        else{
+            jLabel1.setText("Off");
+        }
+    }
+    
+    public boolean getAlarm(){
+        return jLabel1.getText().equals("On");
+    }
+    
+    public void startGUI(AlarmGUI ccGUI) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(AlarmGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(AlarmGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(AlarmGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AlarmGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                ccGUI.setVisible(true);
+            }
+        });
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextArea3 = new javax.swing.JTextArea();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Control Center");
+        setResizable(false);
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Alarm Entity GUI");
+
+        jLabel4.setText("Messages Received: ");
+
+        jTextArea3.setEditable(false);
+        jTextArea3.setColumns(20);
+        jTextArea3.setRows(5);
+        jScrollPane3.setViewportView(jTextArea3);
+
+        jLabel5.setText("Alarm:");
+
+        jLabel1.setText("Off");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 455, Short.MAX_VALUE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 220, Short.MAX_VALUE)
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1)
+                        .addGap(27, 27, 27))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(11, 11, 11)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTextArea jTextArea3;
+    // End of variables declaration//GEN-END:variables
+
+}
